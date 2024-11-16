258876b803039ed0d1959df49908db4fcfe20e73
Dijeh Obinna <dijehobinna@gmail.com>
2024-11-16 15:05:18 +0100

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/task_manager_portfolio/',
+  base: '/task_manager_portfolio/',
   plugins: [react()],
   
 })
